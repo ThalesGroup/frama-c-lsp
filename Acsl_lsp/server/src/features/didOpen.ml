27d8eb26c0b43d05684cb107f7b6fd1a94884807
Djamila Mohamed <djamila.mohamed@thalesgroup.com>
2024-06-26 11:00:44 +0200

    added LSP didOpen feature

---
+++ Acsl_lsp/server/src/features/didOpen.ml
@@ -0,0 +1,26 @@
+let handle (req : Types.NotificationMessage.t) : unit = 
+  let req_json = (Types.NotificationMessage.json_of_t req) in
+  let filename =  Utils.remove_file_scheme (Utils.remove_newline (Utils.remove_quotes (Json.save_string (Json.field "uri" (Json.field "textDocument" (Json.field "params" req_json)))))) in
+  
+  let files = [File.from_filename (Filepath.Normalized.of_string (Filepath.normalize filename))] in
+  
+  (* clear last version of the project created with the same filename *)
+  let projects = Project.find_all filename in 
+  List.iter (fun prj ->
+    Project.set_current prj;
+  (* clear AST *)
+    Project.clear ();
+  ) projects;
+
+  let project = Project.create filename in 
+  Project.set_current project;
+
+  (* nb : include paths must be set for new projects *)
+  Configuration.set_includePaths (Configuration.(!configs));
+
+  try
+    ignore (File.init_from_c_files files);
+  with Log.AbortError msg ->
+    Printf.printf "retrieved string : %s\n%!" msg
+
+  
