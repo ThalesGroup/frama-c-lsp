a67a70c5cd9a01e8bd3e987acf6db6530679d514
Adel DJOUDI <adel.djoudi@thalesgroup.com>
2025-01-08 11:31:57 +0100

    cmd

--- Acsl_lsp/client/src/extension.ts
+++ Acsl_lsp/client/src/extension.ts
@@ -222,11 +222,14 @@ class MyTreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
 				let file = item_list[2];
 				let line = item_list[3];
 				// let t_item = new TreeItem(verdict + " : " + property);
-				let t_item = new TreeItem(item.trim(), "showPOVC");
+				let t_item = new TreeItem(item.trim());
 				const workspacePath = workspace.workspaceFolders[0].uri.fsPath;
 				t_item.command = {
 					command: 'vscode.open',
 					arguments: [vscode.Uri.parse(workspacePath + "/" + file + "#L" + line)]
+				} as vscode.Command;
+				t_item.command = {
+					command: 'showPOVC'
 				} as vscode.Command;
 				this.data.push(t_item);
 			});
