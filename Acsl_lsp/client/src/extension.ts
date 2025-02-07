2775fa5f49b65f8726d1ee99970f4cb3d6f2b4be
Adel DJOUDI <adel.djoudi@thalesgroup.com>
2025-02-07 11:47:18 +0100

    updated client

--- Acsl_lsp/client/src/extension.ts
+++ Acsl_lsp/client/src/extension.ts
@@ -729,14 +729,14 @@ async function get_workspace(){
 			const filePath = fileUri.fsPath;
 			const dirname = path.dirname(filePath);
 			const workspaceDir = {uri: vscode.Uri.file(dirname)};
-			vscode.workspace.updateWorkspaceFolders(0, 0, workspaceDir);
+			vscode.workspace.updateWorkspaceFolders(workspace.workspaceFolders ? workspace.workspaceFolders.length : 0, null, workspaceDir);
 			const workspacePath = workspace.workspaceFolders[0].uri.fsPath;
 			create_frama_c_folder(workspacePath);
 			return workspaceDir;
 		} else {
 			const dirname = path.resolve(__dirname);
 			const workspaceDir = {uri: vscode.Uri.file(dirname)};
-			vscode.workspace.updateWorkspaceFolders(0, 0, workspaceDir);
+			vscode.workspace.updateWorkspaceFolders(workspace.workspaceFolders ? workspace.workspaceFolders.length : 0, null, workspaceDir);
 			const workspacePath = workspace.workspaceFolders[0].uri.fsPath;
 			create_frama_c_folder(workspacePath);
 			return workspaceDir;
